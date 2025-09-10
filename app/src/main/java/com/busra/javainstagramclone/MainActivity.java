3b9430f22210c773aaa250f30906d31021e27e89
busragazel <busragzl96@gmail.com>
2025-09-10 23:48:24 +0300

    Basit Kullanıcı Arayüzleri Eklendi

--- app/src/main/java/com/busra/javainstagramclone/MainActivity.java
+++ app/src/main/java/com/busra/javainstagramclone/MainActivity.java
@@ -1,6 +1,7 @@
 package com.busra.javainstagramclone;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void signInClicked(View view){
+
+    }
+
+    public void signUpClicked(View view){
 
     }
 }
---
+++ app/src/main/java/com/busra/javainstagramclone/UploadActivity.java
@@ -0,0 +1,29 @@
+package com.busra.javainstagramclone;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_upload);
+
+    }
+
+    public void uploadButtonClicked(View view){
+
+    }
+
+    public void selectImageClicked(View view){
+
+    }
+}
